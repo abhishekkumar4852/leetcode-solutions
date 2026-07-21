9aff14278121d62ca6a4a8f5008c4ebb88879451
Abhishek kr Gond <abhishekkumar561365@gmail.com>
2026-07-21 20:50:12 +0530

    Time: 115 ms (36.40%) | Memory: 34.4 MB (23.48%) - LeetSync

---
+++ 87-scramble-string/scramble-string.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    unordered_map<string, bool> mp;
+
+    bool solve(string a, string b) {
+
+        if (a.compare(b) == 0)
+            return true;
+
+        if (a.length() <= 1)
+            return false;
+
+        string key = a + " " + b;
+
+        if (mp.find(key) != mp.end())
+            return mp[key];
+
+        int n = a.length();
+        bool flag = false;
+
+        for (int i = 1; i <= n - 1; i++) {
+
+            // Swapped case
+            if (solve(a.substr(0, i), b.substr(n - i, i)) &&
+                solve(a.substr(i, n - i), b.substr(0, n - i))) {
+                flag = true;
+                break;
+            }
+
+            // Not swapped case
+            if (solve(a.substr(0, i), b.substr(0, i)) &&
+                solve(a.substr(i, n - i), b.substr(i, n - i))) {
+                flag = true;
+                break;
+            }
+        }
+
+        return mp[key] = flag;
+    }
+
+    bool isScramble(string s1, string s2) {
+
+        if (s1.length() != s2.length())
+            return false;
+
+        if (s1.empty() && s2.empty())
+            return true;
+
+        mp.clear();
+
+        return solve(s1, s2);
+    }
+};
