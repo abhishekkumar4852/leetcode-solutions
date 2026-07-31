6bf76483a45d27cda39002ec302b68a5f48be4c7
Abhishek kr Gond <abhishekkumar561365@gmail.com>
2026-07-31 06:29:15 +0530

    Time: 3 ms (34.13%) | Memory: 10.7 MB (40.31%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string>words;
+        int n = s.length();
+
+        for(int i = 0; i < n;){
+            while( i < n && s[i] == ' ')
+            i++;
+
+            if( i >= n)
+            break;
+
+            string word = "";
+            while( i < n && s[i] != ' '){
+                word += s[i];
+                i++;
+            }
+            words.push_back(word);
+        }
+        string ans = "";
+        for(int i = words.size()-1; i >= 0 ; i--){
+            ans += words[i];
+
+
+            if( i!= 0)
+              ans += " " ;
+
+        }
+        return ans;
+        
+    }
+};
